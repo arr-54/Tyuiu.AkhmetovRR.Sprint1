3ebf43311b7b07fbb53f63e8d322f8cec299839b
Your Name <you@example.com>
2024-11-22 23:16:05 +0500

    first commit

--- Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test/obj/Debug/net8.0/Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test.AssemblyInfo.cs
+++ Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test/obj/Debug/net8.0/Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab5c0b79c733c462429df057780a540bb833612")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.AkhmetovRR.Sprint1.Task3.V3.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
